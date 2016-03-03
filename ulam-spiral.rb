b182eab012ea9eddaa29529013d73c245dafca83
supernoir <kawohl.bentodesign@gmail.com>
2016-03-03 23:00:15 +0100

    Uses Unicode Blocks and the Paint Gem to differentiate prime numbers

--- ulam-spiral.rb
+++ ulam-spiral.rb
@@ -5,12 +5,13 @@ require 'prime'
 require 'paint'
 
 i = 1
+n = "\u25AA".encode('utf-8')
 
-for i in 1..600 do
+for i in 1..104729 do
  if Prime.prime?(i) == true
-    print Paint["[#{i}]", :red]
+    print Paint[n,"#f00"]
     else
-    print "[#{i}]"
+    print Paint[n,"#fff"]
  end
 end
 
